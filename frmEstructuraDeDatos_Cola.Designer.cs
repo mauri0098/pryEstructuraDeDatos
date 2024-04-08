4ddb788318a63cc005a4474b3bde93a5e943794d
mauri0098 <maurirodriguez0098@gmail.com>
2024-04-07 21:27:48 -0300

    2005

---
+++ frmEstructuraDeDatos_Cola.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace pryEstructuraDeDatos
+{
+    partial class frmEstructuraDeDatos_Cola
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmEstructuraDeDatos_Cola
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmEstructuraDeDatos_Cola";
+            this.Text = "Estructura De Datos: Cola";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
